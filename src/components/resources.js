ada7e789b8b4b84f27b95c30bb234080830ded21
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-13 13:18:19 -0500

    allows users to sign in/out

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipie
+      <ul>
+        <li>1 Cup Salt</li>
+        <li>2 Cups Pepper</li>
+        <li>3 Cups Butter</li>
+      </ul>
+    </div>
+  );
+};
